30a3975934f12339c43c8c96378bebcedc7c1cb1
Insane <81003264+KingInsane46@users.noreply.github.com>
2023-02-16 15:20:27 -0600

    Add files via upload

--- Snake/src/Main.java
+++ Snake/src/Main.java
@@ -1,57 +1,84 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws InterruptedException {
         final Dimension SCREEN_DIMENSION = new Dimension(Global.WORLD_SCALE*Global.SCREEN_SIZE, Global.WORLD_SCALE*Global.SCREEN_SIZE);
+        final GameObject apple = new GameObject(Global.WORLD_SCALE, new Color(255, 0 ,0));
+        final GameObject head = new GameObject(Global.WORLD_SCALE, new Color(0, 100, 0));
+        final GameObject body = new GameObject(Global.WORLD_SCALE, new Color(0 , 200, 0));
+        List<GameObject> bodyParts = new ArrayList<>();
 
         JFrame frame = new JFrame("Snake");
-        frame.add(new GamePanel());
+        JPanel panel = new JPanel()
+        {
+            @Override
+            protected void paintComponent(Graphics g)
+            {
+                super.paintComponent(g);
+                setBackground(new Color(0));
+
+                head.draw(g);
+                apple.draw(g);
+                for(GameObject part : bodyParts)
+                    part.draw(g);
+            }
+        };
         frame.addKeyListener(new GameKeyListener());
+        frame.add(panel);
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(SCREEN_DIMENSION);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-    }
 
-}
+        boolean gameRunning = true;
+        while(gameRunning)
+        {
+            panel.repaint();
+            TimeUnit.MILLISECONDS.sleep(500);
 
-class GamePanel extends JPanel
-{
-    final GameObject apple = new GameObject(Global.WORLD_SCALE, new Color(255, 0 ,0));
-    final GameObject head = new GameObject(Global.WORLD_SCALE, new Color(0, 100, 0));
-    final GameObject body = new GameObject(Global.WORLD_SCALE, new Color(0 , 200, 0));
-
-    List<GameObject> bodyParts = new ArrayList<>();
-
-    public GamePanel()
-    {
-        head.setPosition(Global.SCREEN_SIZE*Global.WORLD_SCALE/2,Global.SCREEN_SIZE*Global.WORLD_SCALE/2);
-    }
-
-    public void paintComponent(Graphics g)
-    {
-        head.draw(g);
-        apple.draw(g);
-        for(GameObject part : bodyParts)
-            part.draw(g);
+            switch(Global.moveDirection)
+            {
+                case 'w':
+                    head.addPosition(0, Global.WORLD_SCALE*-1);
+                    break;
+                case 'a':
+                    head.addPosition(Global.WORLD_SCALE*-1, 0);
+                    break;
+                case 's':
+                    head.addPosition(0, Global.WORLD_SCALE);
+                    break;
+                case 'd':
+                    head.addPosition(Global.WORLD_SCALE, 0);
+                    break;
+            }
+        }
     }
 }
 
 class GameKeyListener extends KeyAdapter
 {
-    char keyPressed;
+    char lastKeyPressed = ' ';
+
     @Override
     public void keyPressed(KeyEvent event)
     {
-        keyPressed = event.getKeyChar();
+        char keyPressed = event.getKeyChar();
+        if(keyPressed == 'w' && lastKeyPressed != 's')
+            lastKeyPressed = keyPressed;
+        else if(keyPressed == 's' && lastKeyPressed != 'w')
+            lastKeyPressed = keyPressed;
+        else if(keyPressed == 'a' && lastKeyPressed != 'd')
+            lastKeyPressed = keyPressed;
+        else if(keyPressed == 'd' && lastKeyPressed != 'a')
+            lastKeyPressed = keyPressed;
+        Global.moveDirection = lastKeyPressed;
+        System.out.println(Global.moveDirection);
     }
 }
 
@@ -79,9 +106,16 @@ class GameObject
         this.x = x;
         this.y = y;
     }
+
+    public void addPosition(int x, int y)
+    {
+        this.x += x;
+        this.y += y;
+    }
 }
 
 class Global
 {
     static int WORLD_SCALE = 10, SCREEN_SIZE = 50;
+    static char moveDirection = 'd';
 }
